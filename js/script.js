95b2f14363dd6da55a9deea48b95cbcef06e33f9
AnalyticAce <dossehdosseh14@gmail.com>
2024-01-31 20:07:18 +0100

    [add] add animation and responsivity

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+// scroll sections
+let sections = document.querySelectorAll("section");
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            //active navbar links
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id +']').classList.add('active');
+            })
+        }
+    });
+    
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100)
+
+    // remove toggle icon and navbar
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+
+
