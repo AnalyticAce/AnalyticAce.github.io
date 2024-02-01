a07cfa95469cfe4b3e81966c38f7ac6f2ae7bbcd
AnalyticAce <dossehdosseh14@gmail.com>
2024-02-01 01:27:51 +0100

    add animation

--- js/script.js
+++ js/script.js
@@ -23,7 +23,12 @@ window.onscroll = () => {
             navlinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id +']').classList.add('active');
-            })
+            });
+            // active sections for animation on scroll
+            sec.classList.add("show-animate");
+        }
+        else {
+            sec.classList.remove("show-animate");
         }
     });
     
