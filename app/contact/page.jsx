1798a6d174060172dfbbe20b64546b09e8471b66
AnalyticAce <dossehdosseh14@gmail.com>
2024-09-09 00:04:56 +0100

    [ADD] contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -43,7 +43,7 @@ const Contact = () => {
             <div className="xl:w-[54%] order-2 xl:order-none">
                 <form className="flex flex-col gap-6 p-10 bg-[#27272c]
                 rounded-xl">
-                    <h3 className="text-4xl text-accent">Let's work together</h3>
+                    <h3 className="text-4xl text-accent">Let&apos;s work together</h3>
                     <p className="text-white/60">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad natus nostrum tempore rerum quia nisi veritatis deleniti nam vero? Possimus omnis similique dolorum fugit aliquam sequi, rerum eaque perspiciatis nobis!</p>
                     {/* name */}
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
